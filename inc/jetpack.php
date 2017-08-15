f1e8d2c57fa49fcf23219a0a7d54dece784485a1
African Hope <samuelguebo@gmail.com>
2017-08-15 21:06:05 +0000

    renaming slug in jetpack.php

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -13,23 +13,23 @@ Jetpack Compatibility File
 ================================================================================================
 */
 
-function iam_jetpack_setup() {
+function maria_jetpack_setup() {
 	// Add theme support for Infinite Scroll.
 	add_theme_support( 'infinite-scroll', array(
 		'container' => 'main',
-		'render'    => 'iam_infinite_scroll_render',
+		'render'    => 'maria_infinite_scroll_render',
 		'footer'    => 'page',
 	) );
 
 	// Add theme support for Responsive Videos.
 	add_theme_support( 'jetpack-responsive-videos' );
 }
-add_action( 'after_setup_theme', 'iam_jetpack_setup' );
+add_action( 'after_setup_theme', 'maria_jetpack_setup' );
 
 /**
  * Custom render function for Infinite Scroll.
  */
-function iam_infinite_scroll_render() {
+function maria_infinite_scroll_render() {
 	while ( have_posts() ) {
 		the_post();
 		if ( is_search() ) :

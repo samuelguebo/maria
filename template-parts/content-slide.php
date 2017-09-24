10673278ccd78ff56825be8f4b2d1def52ba5dae
African Hope <samuelguebo@gmail.com>
2017-09-24 17:30:58 +0000

    using get_field instead of array

--- template-parts/content-slide.php
+++ template-parts/content-slide.php
@@ -20,14 +20,16 @@ Template part for displaying a slide item in the Slider loop
                 <h4 class="post-item-description"><?php echo $post['slide_title'];?></h4>
                 <h2 class="post-item-title"><?php echo $post['slide_description'];?></h2>
 				<h3>
-                    <a href="<?php echo get_the_permalink($post['slide_page']);?>" class="button radius">
-    					<?php echo $post['slide_button_text'];?>
-    				</a>
+                    
                 <h3>
                     
         <div class="large-7 small-8 large-centered summary columns">
-            <a href="<?php echo get_permalink( get_page_by_path( 'etudes' ) );?>" class="small button round navy"><?php _e('Etudes réalisées','alliancelab')?></a>
-            <a href="<?php echo get_permalink( get_page_by_path( 'commander' ) );?>" class="small button round green right"><?php _e('Commander une étude','alliancelab')?></a>
+            <a href="<?php echo get_the_permalink(get_field('slide_page_1', get_ID()));?>" class="button radius left">
+    					<?php echo $post['slide_button_text_1'];?>
+    				</a>
+            <a href="<?php echo get_the_permalink($post['slide_page_1']);?>" class="button radius right">
+    					<?php echo $post['slide_button_text_1'];?>
+    				</a>
 
         </div>
             </div>

00165c254ae421a7f258d35cdf8ad602487f194c
African Hope <samuelguebo@gmail.com>
2018-01-08 23:32:49 +0000

    css update

--- template-parts/section-services.php
+++ template-parts/section-services.php
@@ -16,19 +16,19 @@ $services = Maria_Kirki::get_option( 'services_repeater');
 ?>
 <section class="row main-row clearfix white-section">
 			<section class="columns main-column white-section-intro">
-				<h5 class="section-service-title"><?php maria_highlight_first_word($service_section_title); ?></h5>
-				<p class="section-service-description"><?php echo $service_section_description; ?></p>
+				<h3 class="section-title"><?php echo $service_section_title; ?></h3>
+				<p class="section-description"><?php echo $service_section_description; ?></p>
 			</section><!--section description/-->
 			<?php if(is_array($services)): //make sure array is not empty ?>
 				<section class="columns main-column">
 					<?php foreach ($services as $service): // loop through array?>
-							<div class="small-6 medium-3 large-3 columns service">
+							<div class="small-6 medium-3 large-3 columns section-item">
 								<a href="<?php the_permalink($service['service_page']);?>" title="<?php echo $service['service_title'];?>">
 									<h2 class="service-icon">
 										<i class="fa fa-<?php echo $service['service_icon'] ?>"></i>
 									</h2>
-									<h5 class="service-title"><?php echo $service['service_title'] ?></h5>
-									<p class="service-description"><?php echo $service['service_description'] ?>
+									<h5 class="item-title"><?php echo $service['service_title'] ?></h5>
+									<p class="item-description"><?php echo $service['service_description'] ?>
 									</p>
 								</a>
 							</div>

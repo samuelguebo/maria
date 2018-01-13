e236a30b105501a7698246a4544a02ff2e51fcd4
African Hope <samuelguebo@gmail.com>
2018-01-13 23:50:34 +0000

    white-section width

--- template-parts/section-services.php
+++ template-parts/section-services.php
@@ -14,23 +14,25 @@ $service_section_title = Maria_Kirki::get_option( 'services_section_title');
 $service_section_description = Maria_Kirki::get_option( 'services_section_description');
 $services = Maria_Kirki::get_option( 'services_repeater');
 ?>
-<section class="row main-row clearfix white-section large-7 small-8 large-centered columns">
-			<section class="columns main-column white-section-intro">
+<section class="row main-row clearfix white-section">
+			<section class="columns main-column white-section-intro large-8 small-8 large-centered columns">
 				<h3 class="section-title"><?php echo $service_section_title; ?></h3>
 				<p class="section-description"><?php echo $service_section_description; ?></p>
 			</section><!--section description/-->
 			<?php if(is_array($services)): //make sure array is not empty ?>
 				<section class="columns main-column">
 					<?php foreach ($services as $service): // loop through array?>
-							<div class="small-6 medium-3 large-3 columns section-item">
-								<a href="<?php the_permalink($service['service_page']);?>" title="<?php echo $service['service_title'];?>">
+							<div class="small-6 medium-4 large-4 columns section-item">
+								
 									<h2 class="service-icon">
-										<i class="fa fa-<?php echo $service['service_icon'] ?>"></i>
+										
 									</h2>
 									<h5 class="item-title"><?php echo $service['service_title'] ?></h5>
 									<p class="item-description"><?php echo $service['service_description'] ?>
 									</p>
-								</a>
+                                <a href="<?php the_permalink($service['service_page']);?>" title="<?php echo $service['service_title'];?>">
+                                <i class="fa fa-chevron-right"></i>
+                                </a>
 							</div>
 					<?php endforeach;?>
 				</section>
